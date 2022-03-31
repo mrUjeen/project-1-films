5bf5c99bdad46f5f742bb5f2858f0c0582adc0cd
mrUjeen <yevhen.lupych@gmail.com>
2022-03-31 13:08:01 +0300

    project-films-2.2.3

--- script.js
+++ script.js
@@ -37,7 +37,7 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -58,8 +58,16 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
+}
 
-
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
 
 console.log(personalMovieDB);
