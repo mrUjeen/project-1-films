29149ca6d858d010636c4b23a359a4083c0b3ccf
mrUjeen <yevhen.lupych@gmail.com>
2022-03-31 13:30:21 +0300

    project-films-2.2.4

--- script.js
+++ script.js
@@ -37,7 +37,7 @@
 
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -47,7 +47,21 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b!= null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+let i = 0;
+while (i < 2) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
@@ -58,6 +72,7 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
+    i++;
 }
 
 if (personalMovieDB.count < 10) {
