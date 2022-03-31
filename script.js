cbd44fac3da62b1617b18daa6b122e56b9154ec7
mrUjeen <yevhen.lupych@gmail.com>
2022-03-31 12:48:40 +0300

    project-films-2.2.2

--- script.js
+++ script.js
@@ -47,11 +47,18 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
-          personalMovieDB.movies[a] = b;
+    if (a != null && b!= null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
 
 }
 
